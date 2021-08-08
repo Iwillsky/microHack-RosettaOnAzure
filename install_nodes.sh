83043650599e1d3e1ba398ec48c9f5e4ae4ca161
iwillsky <45908192+Iwillsky@users.noreply.github.com>
2021-08-08 15:17:07 +0800

    Update scripts

--- install_nodes.sh
+++ install_nodes.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
 
 ### Boot a VM using CentOS-based HPC 7.9 Gen2
-### Resize OS disk(Stop VM; Set sys disk as 64G; start VM)
-### SSH login
+### Resize OS disk(Stop VM; Set OS disk at least 64G; start VM)
+### SSH login 
 
-## install anaconda 
+## Install anaconda 
 wget https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
 chmod +x Anaconda3-2021.05-Linux-x86_64.sh
 sudo bash ./Anaconda3-2021.05-Linux-x86_64.sh
-#read license with blank to next page
-#set the destination dir as /opt/anaconda3
-#select 'yes' when ask if need conda init
+# read license with blank to next page
+# set the destination dir as /opt/anaconda3
+# select 'yes' when ask if need conda init
 cat <<EOF | sudo tee -a /etc/profile
 export PATH=\$PATH:/opt/anaconda3/bin
 EOF
 source /etc/profile
 
-## Get repo and build 
-cd /opt/anaconda3
+## Get repo and setup conda env 
+cd /opt
 sudo su
-git clone https://github.com/RosettaCommons/RoseTTAFold.git
+conda deactivate    #back to VM shell
+git clone https://github.com/Iwillsky/RoseTTAFold.git    #branch from RosettaCommons/RoseTTAFold modified for HPC env
 cd RoseTTAFold
 conda env create -f RoseTTAFold-linux.yml
 conda env create -f folding-linux.yml
 conda env list
 ./install_dependencies.sh
 
+## Install pyrosetta in folding env
 conda init bash
-source /home/azureuser/.bashrc
+source ~/.bashrc
 conda activate folding
+# original download link: https://www.pyrosetta.org/downloads 
+# Register first. Below is a copy, while download means obey the license requirements at https://els2.comotion.uw.edu/product/pyrosetta
 wget https://proteinfoldonazure.blob.core.windows.net/data/PyRosetta4.Release.python37.linux.release-289.tar.bz2
-tar -vjxf PyRosetta4.Release.python37.linux.release-289.tar.bz2 
+tar -vjxf PyRosetta4.Release.python37.linux.release-289.tar.bz2
 cd PyRosetta4.Release.python37.linux.release-289/setup
 python setup.py install
-#verify the pyrosetta lib
-python   #then input two lines:  <<<import pyrosetta;   <<<pyrosetta.init()
-conda deactivate   #back to conda (base)
+# [Optional] verify the pyrosetta lib
+# python   #then input two lines:  <<<import pyrosetta;   <<<pyrosetta.init()
+conda deactivate    #back to conda (base)
 conda deactivate    #back to VM shell
 
--- runjob.sh
+++ runjob.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #SBATCH -o job%j.out
-#SBATCH --job-name=Fold
-#SBATCH --nodes=1       
-## #SBATCH --gres=gpu:1
+#SBATCH --job-name=RosettaOnAzure
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=96G
+#SBATCH --gres=gpu:1
+JOBID=$SLURM_ARRAY_JOB_ID
+WORKDIR=/shared/home/cycleadmin/RoseTTAFold
 
-/opt/RoseTTAFold/run_pyrosetta_ver.sh /share/home/cycleadmin/input.fa /share/home/cycleadmin/
+$WORKDIR/run_pyrosetta_ver.sh $WORKDIR/input.fa $WORKDIR $JOBID 16 96
+#$WORKDIR/run_e2e_ver.sh $WORKDIR/input.fa $WORKDIR $JOBID 16 96
